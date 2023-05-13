60f92d6e99e72100018d7b29f5a339647f1b1af7
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-05-12 20:29:30 -0500

    validar email DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,45 @@
 const { Router, response } = require('express');
-const { validationResult } = require('express-validator');
+const Usuario = require('../models/usuario');
 
-const crearUsuario = (req, res= response) => {
+const crearUsuario = async (req, res= response) => {
 
+    const { email } = req.body;
 
+ 
+    try {
+
+        const existeEmail =  await Usuario.findOne({ email });
+
+        console.log('============+++++++++++++++++++++++=============================');;
+        console.log(email);
+        console.log('============*++++++++++++++++++++++==========================');;
+    
+        if(existeEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya esta registrado'
+            });
+        }
+        
+       
+
+        const usuario = new Usuario(req.body);
+        await usuario.save()
 
         res.json({
             ok: true,
-            msg: ' Crear usuario!!!'
+            usuario
         })
+        
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
     
 }
 
--- database/config.js
+++ database/config.js
@@ -12,8 +12,6 @@ const dbConnection = async() => {
 }
 
 
-
-
 module.exports = {
     dbConnection
 }
