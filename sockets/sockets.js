4f336f721ea606650c0a1e84f726d7fa70305cf9
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-05-15 17:07:33 -0500

    actualizar campo online desde socket

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,19 +1,23 @@
+const { usuarioDisconnect, usuarioConnectado } = require('../controllers/socket');
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 const { emit } = require('nodemon');
 
-io.on('connect', client => {
+io.on('connect',  (client) => {
     
     //handshake apreton de mano en ingles
     // console.log(client.handshake.headers['x-token']);
     
-    console.log("Cliente Conectado");
+    console.log(" Conectando...");
     const  [valido, uid] = comprobarJWT(client.handshake.headers['x-token']) 
     if(!valido){ return client.disconnect(); }
 
+     usuarioConnectado(uid);
+
     console.log('Cliente autentucado');
 
-    client.on('disconnect', () => { 
+    client.on('disconnect',  () => { 
+         usuarioDisconnect(uid);
         console.log("Cliente desconectado");
      });
   });
